730c07e7796dba1c3b9a3be5f2b020ffdaa4f86c
Arghun <arghununiversity@gmail.com>
2019-08-31 23:36:36 +0430

    Updates

---
+++ precache-manifest.32294d9b611cd08b9f8abe5b64d9e2c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7c0ccbca6537f6b36545130f418569",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "43a5a4a5142aaab176d0",
+    "url": "/MonstersRolodex/static/css/main.0a718600.chunk.css"
+  },
+  {
+    "revision": "bc2d644bac4bad03e3aa",
+    "url": "/MonstersRolodex/static/js/2.931ff8cf.chunk.js"
+  },
+  {
+    "revision": "43a5a4a5142aaab176d0",
+    "url": "/MonstersRolodex/static/js/main.c4e354a2.chunk.js"
+  },
+  {
+    "revision": "0afc7af1385e65d3ec63",
+    "url": "/MonstersRolodex/static/js/runtime~main.bdfd9016.js"
+  }
+]);
